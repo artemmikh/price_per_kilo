1d1d0ba6fca45b268808beaf85b5742efd97309f
Михайлов Артём <artem.mikh100@gmail.com>
2024-09-30 14:53:40 +0300

    Добавление основной логики, добавление функции проверки и парсинга сообщения

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,20 +1,22 @@
+from bot.test import message
+from bot.utils import check_message, parse_message
 from utils import send_message
+
+INFO_MESSAGE = ('Пожалуйста, отправьте в чат две цифры через пробел, '
+                'напишите сначала вес в граммах, затем сумму. '
+                'Используйте целые числа. Например "820 370".')
 
 
 def wake_up(update, context):
     send_message(update, context,
                  # TODO При добавлении float изменить сообщение
-                 message=('Добро пожаловать! Пожалуйста, отправьте в чат две цифры через пробел, '
-                          'напишите сначала вес в граммах, затем сумму. '
-                          'Используйте целые числа. Например "820 370".'))
+                 message=(f'Добро пожаловать! {INFO_MESSAGE}'))
 
 
 def price_per_kilo(update, context):
-    # TODO добавить проверку соответствия патерну
-    # TODO распарсить сообщение
-    # TODO добавить обработку float
-    # price = 0
-    # grams = 0
-    # text =  f'Цена за килограмм – {int(price / (grams / 1000))} руб'
-
-    send_message(update, context, message='это текст из price_per_kilo')
+    message = update.message.text
+    if check_message(message) is None:
+        send_message(update, context, f'Проверьте сообщение. {INFO_MESSAGE}')
+        return
+    grams, price = parse_message(message)
+    send_message(update, context, message=f'Цена за килограмм – {int(price / (grams / 1000))} рублей')
--- bot/utils.py
+++ bot/utils.py
@@ -1,4 +1,5 @@
 from logger import logger
+import re
 
 
 def send_message(update, context, message):
@@ -9,3 +10,16 @@ def send_message(update, context, message):
         logger.debug(f'Сообщение успешно отправлено в Telegram: {message}')
     except Exception as error:
         logger.error(f'Сбой при отправке сообщения в Telegram: {error}')
+
+
+def check_message(message):
+    return re.match(r'\d{1,15} \d{1,15}', message)
+
+
+def parse_message(message):
+    return int(message.split(' ')[0]), int(message.split(' ')[1])
+
+
+a, b = parse_message('12 122')
+
+print(type(a))
